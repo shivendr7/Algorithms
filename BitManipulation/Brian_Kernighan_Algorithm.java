e135a114445d9f8e13a392e53c7b60ce3727fb81
Shivendra Pratap Singh <59742488+shivendr7@users.noreply.github.com>
2023-04-05 22:19:36 +0530

    Update Brian_Kernighan_Algorithm.java

--- BitManipulation/Brian_Kernighan_Algorithm.java
+++ BitManipulation/Brian_Kernighan_Algorithm.java
@@ -1,1 +1,24 @@
 
+// Counting number of 1s in the binary representation of a number
+
+/**
+
+x = 5 (101)2 
+Step 1: 
+x = 5 (101)2 
+x-1 = 4 (100)2 
+x & (x-1) = (100)2 = 4, 
+update x to 4. 
+Step 2: x = 4 (100)2 
+x-1 = 3 (011)2, 
+x & (x-1) = (000)2 = 0 
+So, the number of set bits are 2 in x.
+
+**/
+
+int n;
+int c = 0;
+while (n>0) {
+  n = n&(n-1);
+  c++;
+}
